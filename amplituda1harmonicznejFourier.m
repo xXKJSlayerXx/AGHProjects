4773cadc61bc660b56ecc0cb6308511b18da9e63
xXKJSlayerXx <103508057+xXKJSlayerXx@users.noreply.github.com>
2023-03-05 14:45:17 +0100

    Delete amplituda1harmonicznejFourier.m

--- amplituda1harmonicznejFourier.m
+++
@@ -1,7 +0,0 @@
-                                                                                                     %	Dany jest zespolony szereg Fouriera sygnału rzeczywistego Podaj amplitudę pierwszej harmonicznej rzeczywistej wiedząc, że X(1) = 1+2i .
-clc;
-clear;
-%x+yi=z
-x=1;
-yi=1;
-wynik=2*sqrt(x^2+yi^2);  
