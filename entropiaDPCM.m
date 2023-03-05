3e9879e3b367a6c96acf720915baf5ace78d4ff4
xXKJSlayerXx <103508057+xXKJSlayerXx@users.noreply.github.com>
2023-03-05 14:45:37 +0100

    Delete entropiaDPCM.m

--- entropiaDPCM.m
+++
@@ -1,7 +0,0 @@
-clear all;
-close all; 
-clc;
-d=[-186, -155, -124, -93, -62, -31, 0, 31, 62, 93, 124]; %współczynniki
-n=[1, 2, 23, 120, 494, 8734, 46543, 8776, 730, 98, 15]; %liczebność
-pi=n/sum(n);
-H=-sum(pi.*log2(pi)) %entropia
